947cfb5bbe40e6975f569a7b21f18977d804bc38
pedrolls <silva418@hotmail.com>
2022-01-19 20:10:21 -0300

    Adição das Classe de dominio Cidade e Estado

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -6,9 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Cidade;
+import com.nelioalves.cursomc.domain.Estado;
 import com.nelioalves.cursomc.domain.Produto;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.repositories.CidadeRepository;
+import com.nelioalves.cursomc.repositories.EstadoRepository;
 import com.nelioalves.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +24,12 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	ProdutoRepository produtoRepository;
+	
+	@Autowired
+	CidadeRepository cidadeRepository;
+	
+	@Autowired
+	EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -42,10 +53,25 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		produtoUm.getCategorias().addAll(Arrays.asList(categoriaUm));
 		produtoDois.getCategorias().addAll(Arrays.asList(categoriaUm,categoriaDois));
-		produtoTres.getCategorias().addAll(Arrays.asList(categoriaUm));
+		produtoTres.getCategorias().addAll(Arrays.asList(categoriaUm));		
 		
 		categoriaRepository.saveAll(Arrays.asList(categoriaUm, categoriaDois));
 		produtoRepository.saveAll(Arrays.asList(produtoUm, produtoDois, produtoTres));
+		
+		//Instanciando Estado e Cidades
+		
+		Estado estadoUm = new Estado(null, "Minas Gerais");
+		Estado estadoDois = new Estado(null, "Uberlândia");
+		
+		Cidade cidadeUm = new Cidade(null, "Uberlandia", estadoUm);
+		Cidade cidadeDois = new Cidade(null, "São Paulo", estadoDois);
+		Cidade cidadeTres = new Cidade(null, "Campinas", estadoDois);
+		
+		estadoUm.getCidades().addAll(Arrays.asList(cidadeUm));
+		estadoDois.getCidades().addAll(Arrays.asList(cidadeDois, cidadeTres));
+		
+		estadoRepository.saveAll(Arrays.asList(estadoUm, estadoDois));
+		cidadeRepository.saveAll(Arrays.asList(cidadeUm, cidadeDois, cidadeTres));
 		
 	}
 
