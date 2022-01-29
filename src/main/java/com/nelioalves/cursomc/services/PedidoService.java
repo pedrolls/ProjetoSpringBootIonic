7f4a339eb939b5f63b54cd2e41b3ab5c3ed19b3d
pedrolls <silva418@hotmail.com>
2022-01-28 21:30:50 -0300

    Endpoint pedido por id implementado.

---
+++ src/main/java/com/nelioalves/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.nelioalves.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Pedido;
+import com.nelioalves.cursomc.repositories.PedidoRepository;
+import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	PedidoRepository repository;
+	
+	public Pedido buscarPedidoPorId(Integer id) {
+		Optional<Pedido> pedido = repository.findById(id);
+		return pedido.orElseThrow(() -> new ObjectNotFoundException("Objeto não encotrado! ID: ".concat(id.toString()).
+				concat(", Tipo: ").concat(Pedido.class.getSimpleName())));
+	}
+
+}
